f3f42b1ffba4c0cbf7bb51693e14e092babec336
brahian-7 <brastipalriv@gmail.com>
2026-08-05 10:04:12 -0500

    Refactor iniciarCarta function
    Refactor iniciarCarta function to improve formatting and logic.

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
-function iniciarCarta(){
-
-    let musica=document.getElementById("musica");
+function iniciarCarta() {
+    let musica = document.getElementById("musica");
 
     musica.play();
 
     alert("Bienvenida mi amor 💙\nEsta página fue creada únicamente para ti.");
+
+    window.location.href = "capitulo1.html";
 }
